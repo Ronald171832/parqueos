9dcb9010636f2f43701fc391a5d63f1cba485756
RONALD\Ronald Lopez <ronald_lopez_o@hotmail.com>
2018-04-18 05:23:51 -0400

    tercer comit

---
+++ app/src/main/java/com/rldevelopers/parqueos/parqueonline/Adm/AdmActivity.java
@@ -0,0 +1,15 @@
+package com.rldevelopers.parqueos.parqueonline.Adm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.rldevelopers.parqueos.parqueonline.R;
+
+public class AdmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.adm_menu_principal);
+    }
+}
---
+++ app/src/main/java/com/rldevelopers/parqueos/parqueonline/Adm/Fragment/RegistroFragment.java
@@ -0,0 +1,49 @@
+package com.rldevelopers.parqueos.parqueonline.Adm.Fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.rldevelopers.parqueos.parqueonline.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class RegistroFragment extends Fragment implements View.OnClickListener {
+
+
+
+
+    public RegistroFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.adm_fragment_registro, container, false);
+      //  prefsLogin = getContext().getSharedPreferences("PrefsLogin", Context.MODE_PRIVATE);
+        BindUi(view);
+        return view;
+    }
+
+    public void BindUi(View view) {
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+           /* case R.id.imgCerrarSesion:
+
+                break;*/
+        }
+    }
+
+}
