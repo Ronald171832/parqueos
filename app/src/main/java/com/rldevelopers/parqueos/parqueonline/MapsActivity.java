2c443401538bc8c946764bd2e761e812f592acd5
RONALD\Ronald Lopez <ronald_lopez_o@hotmail.com>
2018-04-17 23:31:03 -0400

    primer comit

---
+++ app/src/main/java/com/rldevelopers/parqueos/parqueonline/MapsActivity.java
@@ -0,0 +1,76 @@
+package com.rldevelopers.parqueos.parqueonline;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polygon;
+import com.google.android.gms.maps.model.PolygonOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnPolygonClickListener, GoogleMap.OnMapClickListener {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setOnPolygonClickListener(MapsActivity.this);
+        mMap.setOnMapClickListener(MapsActivity.this);
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    @Override
+    public void onPolygonClick(Polygon polygon) {
+        polygon.setFillColor(Color.BLUE);
+        Toast.makeText(MapsActivity.this, polygon.getPoints().get(1) + "", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onMapClick(LatLng latLng) {
+        PolygonOptions rectOptions = new PolygonOptions()
+                .add(new LatLng(37.35, -122.0), new LatLng(37.45, -122.0), new LatLng(37.45, -122.2),
+                        new LatLng(37.35, -122.2),
+                        new LatLng(37.35, -122.0));
+
+        mMap.addPolygon(rectOptions).setClickable(true);
+
+        Polyline line = mMap.addPolyline(new PolylineOptions()
+                .add(new LatLng(-17.7940151, -63.1933257), new LatLng(-17.80, -63.20))
+                .width(0.5f)
+                .color(Color.BLUE)
+                .geodesic(true));
+    }
+}
